58798efad1362d7eea05eb7e44c9b7966fed2702
wogdiser <ivan.rakshi@gmail.com>
2020-12-13 18:49:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Lab 2
+# Вариант 2.
+# Zad. 2
+# С помощью генератора сформируйте словарь в котором ключами являются целые числа, а значениями — суммы цифр ключей
+
+import random
+
+def sum_digits(num):
+  result = 0
+  for char in str(num):
+    result = result + int(char)
+  return result
+
+dict = {}
+
+for i in range(6):
+  key = random.randint(1, 100)
+  dict[key] = sum_digits(key)
+
+print(dict)
